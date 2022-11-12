8baac5b3628346e0cfd641b14e6e891cfc7d1bcb
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-12 09:15:23 +0530

    [Manoj] Add: InventoryManagement

--- Oops/Oops/Program.cs
+++ Oops/Oops/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Oops
+﻿using Oops.InventoryManagement;
+
+namespace Oops
 {
     public class Program
     {
+        static string inventoryFilePath = @"D:\BridgeLabz\Oops\Oops\Oops\Inventory.json";
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Oops Problem");
+            bool flag = true;
+            int check = Convert.ToInt32(Console.ReadLine());
+            while (flag)
+            {
+                switch (check)
+                {
+                    case 1:
+                        InventoryDetailsManagement inventoryDetailsManagement = new InventoryDetailsManagement();
+                        inventoryDetailsManagement.ReadJsonFile(inventoryFilePath);
+                        break;
+                }
+                break;
+            }
         }
     }
 }
